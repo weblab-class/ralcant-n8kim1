a10b07874dbbd18e4f6c3b4fac2cccd4d6ee0a0d
Nathaniel Kim <nkim8@mit.edu>
2019-01-26 13:22:02 -0500

    adding css to game page

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -12,8 +12,6 @@ function renderNavbar(user) {
   navbarDiv.appendChild(newNavbarItem('Home ', '/'));
   // NOTE: this check is a lowkey hack
   if (user._id !== undefined) {
-    //navbarDiv.appendChild(newNavbarItem('Profile', '/u/profile?'+user._id));
-    //navbarDiv.appendChild(newNavbarItem('Home ', '/'));
     navbarDiv.appendChild(newNavbarItem('About','/about'));
     navbarDiv.appendChild(newNavbarItem('Rules ', '/rules'));
     navbarDiv.appendChild(newNavbarItem('Logout ', '/logout'));
@@ -21,40 +19,3 @@ function renderNavbar(user) {
    // navbarDiv.appendChild(newNavbarItem('Login', '/auth/google'));  
   }
 }
-
-// function renderNavbar(user){
-//   const navbarDiv = document.getElementById('nav-item-container');
-//   if (user._id !== undefined){
-//     //const newnav = document.createElement('nav');
-//     //new 
-//     const x = (
-//       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-//         {/* <a class="navbar-brand" href="#">Navbar</a> */}
-//         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//           <span class="navbar-toggler-icon"></span>
-//         </button>
-//         <div class="collapse navbar-collapse" id="navbarNav">
-//           <ul class="navbar-nav">
-//             <li class="nav-item active">
-//               <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-//             </li>
-//             <li class="nav-item">
-//               <a class="nav-link" href="/rules">Rules</a>
-//             </li>
-//             <li class="nav-item">
-//               <a class="nav-link" href="/about">About</a>
-//             </li>
-//             <li class="nav-item">
-//               <a class="nav-link disabled" href="/logout">Logout</a>
-//             </li>
-//           </ul>
-//         </div>
-//       </nav>
-//     )
-//     navbarDiv.appendChild(x);
-//   } else {
-//     navbarDiv.appendChild(newNavbarItem('Login', '/auth/google'));  
-//   }
-
-// }
-  
